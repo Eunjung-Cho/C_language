de51fe19c757141c1e72d0f956867ec1a8c2fba3
Eunjung-Cho <59313007+Eunjung-Cho@users.noreply.github.com>
2021-02-20 22:51:26 +0900

    Add files via upload

---
+++ loop/4week_1.c
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+
+/*
+during : during the winter
+while: ~동안에 (무한루프)
+for: 뒤에 숫자가오는 경우 딱 정의되었을 때 (반복횟수가 정해져있다)
+*/
+
+int main()
+{
+	using namespace std;
+
+	for (int count = 0; count < 10; ++count)
+	{
+		cout << count << ednl;
+	}
+
+	return 0;
+}
